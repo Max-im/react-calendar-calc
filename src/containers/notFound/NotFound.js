753d41f288aeb68db86db4ee55c01a0fb1ecf5cd
Max Pozhidayev <pogidaevmo@gmail.com>
2017-10-25 19:36:49 +0300

    add container 404, create route for 404

---
+++ src/containers/notFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+import './NotFound.css';
+
+function NotFound(props) {
+  return (
+    <div className="header">
+      404
+    </div>
+  );
+}
+
+
+export default NotFound;
+
+
