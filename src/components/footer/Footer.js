51f341564e0258e54891e5e3cd53e94d3e7375da
Max Pozhidayev <pogidaevmo@gmail.com>
2017-10-26 19:46:24 +0300

    make footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './Footer.css';
+import FooterContainer from '../../containers/footerContainer/FooterContainer';
+
+const data = [
+  { name: "Terms", link: "/" },
+  { name: "Help", link: "/" },
+  { name: "Privacy", link: "/" },
+  { name: "Status", link: "/" },
+  { name: "Security", link: "/" },
+  { name: "Contact", link: "/" }
+]
+
+
 
 class Footer extends Component {
   render() {
     return (
      <footer className="footer">
-        footer
+        <div className="container_flex footer__container">
+
+          <div className="footer__copyrightWrap">
+            <small className="footer__copyright">{`© ${new Date().getFullYear()} Copyright`}</small>
+          </div>
+          
+
+          <div className="footer__menu">
+            <FooterContainer data={data} />
+          </div>
+
+        </div>
       </footer>
     );
   }
--- src/containers/footerContainer/FooterContainer.js
+++ src/containers/footerContainer/FooterContainer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './FooterContainer.css';
+import { Link } from 'react-router'
 
 function FoorerContainer(props) {
+  const data = props.data;
+  const listItems = data.map( (item) => 
+    <li key={item.name} className="footer__listItem">
+    <Link to={item.link} className="footer__link">{item.name}</Link>
+    </li>
+  );
+
   return (
-    <footer className="footer">
-      footer
-    </footer>
+    <ul className="footer__list">
+      {listItems}
+    </ul>
   );
 }
 
