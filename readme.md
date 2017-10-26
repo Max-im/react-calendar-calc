e7fd6603945501dd49ffed80ba01ce60e265f850
Max Pozhidayev <pogidaevmo@gmail.com>
2017-10-26 23:25:30 +0300

    begin abbout component

---
+++ readme.md
@@ -0,0 +1,57 @@
+#Calendar-calc
+
+
+##App info:
+ - name:	 	Calendar-calc;
+ - description:		Calculates the number of days between two selected dates;
+ - start date: 		23.10.2017;
+ - version:		1.0.0
+
+
+
+##SetUp the app
+ - git clone https://max-im.github/react-calendar-calc 		( download the project )
+ - cd react-calendar-calc 	( go to the project directory )
+ - npm i 			( for initialisation the project dependencies );
+ 
+
+
+##Run the app
+ - npm start 			( for start the project in develop mode );
+ - npm test			( for testing the project );
+ - npm run build		( for build production version );
+
+
+
+##Technologies:
+ - react-cli;
+ - React / ES6;
+ - Router;
+ - HTML5;
+ - Sass (Scss) / CSS3;
+
+
+
+##Schedule:
+|01.| Consider the project 				| 1 hour	|
+|02.| Environment init, structure the project		| 2 hours	|
+|03.| Routing implementation				| 1 hour	|
+|04.| Create Header component 				| 3 hours	|
+|05.| Create NotFound (404) component			| 1 hours	|
+|06.| Create About component				| 5 hours	|
+|07.| Create Calc component				| 5 hours	|
+|08.| Create Contacts component				| 5 hours	|
+|09.| Create Footer component				| 1 hours	|
+|10.| Refactoring					| 3 hours	|
+|   | TOTAL						| 28 hours	|
+
+
+
+
+##Author info
+author: 	Maxim Pozhidaev <pogidaevmo@gmail.com>
+authorPage:	https://max-im.github.io/
+portfolio:	https://max-im.github.io/portfolio/
+phone: 		+38 (050) 77-23-169
+
+
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
 import './About.css';
+import AboutBlock from '../../containers/aboutBlock/AboutBlock';
+
+
+const data = [
+  { 
+    title: 'SetUp the App',
+    img: 'logo',
+    list: [
+      { name: 'git clone the repo' },
+      { name: 'cd repo dir' },
+      { name: 'npm i' }
+    ]
+  },
+  { 
+    title: 'Run the App',
+    list: [
+      { name: 'npm start - for start App in develop mode' },
+      { name: 'npm test - for test the App' },
+      { name: 'npm run build - for build production version' }
+    ]
+  },
+  { 
+    title: 'App technologies',
+    list: [
+      { name: 'React-cli (create-react-app)' },
+      { name: 'React / ES6' },
+      { name: 'React-router' },
+      { name: 'Sass (Scss) / CSS3' },
+      { name: 'HTML5' }
+    ]
+  }
+]
+
+
+
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: false
+    }
+  }
+
+  visibleToggle(){
+    this.setState({
+      isVisible: !this.state.isVisible
+    })
+  }
+
   render() {
     return (
       <div className="about">
-        about
+        <div className="container about__container">
+        
+          <h2 className="about__header App__header">About this App</h2>
+
+          <div className="about__contentWrap">
+            <AboutBlock 
+              data={data} 
+              click={this.visibleToggle.bind(this)}
+              visible={this.state.isVisible} 
+            />
+          </div>
+
+        </div>
       </div>
     );
   }
