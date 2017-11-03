4850f19f2b9a03310e15cf827e246c5440bc6f96
Max Pozhidayev <pogidaevmo@gmail.com>
2017-11-04 00:37:07 +0200

    finish decorate calendar

--- src/components/calc/Calc.js
+++ src/components/calc/Calc.js
@@ -20,7 +20,6 @@ class Calc extends Component {
 
    
   get(e, el){
-
     if(el.target.closest('.calendarComp').classList.contains('first')){
       this.setState({
         firstDate: moment(e).format('DD/MM/YY'),
@@ -34,6 +33,46 @@ class Calc extends Component {
         rawLast: e,
         result: Math.floor((e.getTime() - this.state.rawFirst.getTime()) /1000/60/60/24)
       }); 
+    }
+    this.decor(e, el)
+  }
+
+  decor(e, el){
+    const first = document.querySelector('.first');
+    const firstList = Array.from(first.querySelectorAll('.calc__calendarDate'));
+    const second = document.querySelector('.second');
+    const secondList = Array.from(second.querySelectorAll('.calc__calendarDate'));
+    
+
+    if(el.target.closest('.first')){
+
+      firstList.forEach( item => item.classList.remove('calc__point'))
+      el.target.classList.add('calc__point');
+      firstList.forEach( item => item.classList.remove('calc__mark'))
+
+    }
+    else{
+      secondList.forEach( item => item.classList.remove('calc__point'))
+      el.target.classList.add('calc__point');
+      secondList.forEach( item => item.classList.remove('calc__mark'))
+    }
+    if( document.querySelectorAll('.calc__point').length === 2 ){
+      
+      siblingsDecor('first');
+      siblingsDecor('second');
+
+
+      function siblingsDecor (clas) {
+        const elem = document.querySelector(`.${clas} .calc__point`);
+        let sibl = clas === 'first' ? elem.nextSibling : elem.previousSibling;
+        const arrFirst = [];
+
+        while(sibl){
+          arrFirst.push(sibl);
+          sibl = clas === 'first' ? sibl.nextSibling : sibl.previousSibling;
+        }  
+        arrFirst.forEach( item => item.classList.add('calc__mark'));  
+      }
     }
   }
 
