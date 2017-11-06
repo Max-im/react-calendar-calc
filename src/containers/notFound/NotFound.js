5be32d6fc917fa2c3eb72c712f06b85eea7bf2fa
Max Pozhidayev <pogidaevmo@gmail.com>
2017-11-06 19:48:17 +0200

    add notFound component

--- src/containers/notFound/NotFound.js
+++ src/containers/notFound/NotFound.js
@@ -4,7 +4,12 @@ import './NotFound.css';
 function NotFound(props) {
   return (
     <div className="notFound">
-      404
+      <div className="container flex">
+        <div className="notFound__content">
+          <h2 className="notFound__txt">page not found</h2>
+          <p className="notFound__err">404 error</p>
+        </div>
+      </div>
     </div>
   );
 }
